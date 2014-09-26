40520dc2a4e65c30641bda5550e6f2b4797ea75d
Murilo Laghi <murilo.laghi@gmail.com>
2014-09-26 01:12:45 -0300

    Implementação do build, implementação de teste unitário inicial

--- test/ppla-builder_test.js
+++ test/ppla-builder_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var ppla_builder = require('../lib/ppla-builder.js');
+var PplaBuilder = require('../lib/ppla-builder.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -22,15 +22,39 @@ var ppla_builder = require('../lib/ppla-builder.js');
     test.ifError(value)
 */
 
-exports['awesome'] = {
-  setUp: function(done) {
-    // setup here
-    done();
-  },
-  'no args': function(test) {
-    test.expect(1);
-    // tests here
-    test.equal(ppla_builder.awesome(), 'awesome', 'should be awesome.');
-    test.done();
-  },
-};
+exports.testSingleLineBuild = {
+    setUp: function(done) {
+        // setup here
+        this.expectedFinalLabel = new Buffer('\u0002L\u000D4:5300101500010Etiqueta de teste\u000DE\u000D', 'ascii')
+        done();
+    },
+    'no args': function(test) {
+        test.expect(1);
+
+        PplaBuilder.rotation(PplaBuilder.DIRECTIONS.LANDSCAPE)
+                   .fontType(PplaBuilder.FONT_TYPE[':'].value)
+                   .fontSubType(PplaBuilder.COURIER_SUBTYPES.ECMA94)
+                   .hScale(5)
+                   .vScale(3)
+                   .x(10)
+                   .y(150)
+                   .label('Etiqueta de teste')
+
+        test.deepEqual(PplaBuilder.build(), this.expectedFinalLabel, 'should be ' + this.expectedFinalLabel);
+        test.done();
+    }
+}
+
+//exports.testMultiLineBuild = {
+//    setUp: function(done) {
+//        // setup here
+//        done();
+//    },
+//    'no args': function(test) {
+//        test.expect(1);
+//        // tests here
+//        test.equal(ppla_builder.awesome(), 'awesome', 'should be awesome.');
+//        test.done();
+//    },
+//}
+
