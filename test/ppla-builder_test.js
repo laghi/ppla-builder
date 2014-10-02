c4a68a1914b9cac5b7aac2e288e13f327de82780
Murilo Laghi <murilo.laghi@gmail.com>
2014-10-01 23:55:37 -0300

    Removed the validation of the barcode type, added some method for configuration of the label style

--- test/ppla-builder_test.js
+++ test/ppla-builder_test.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var PplaBuilder = require('../lib/ppla-builder.js');
+var pplaBuilder = require('../lib/ppla-builder.js');
 
 /*
   ======== A Handy Little Nodeunit Reference ========
@@ -25,21 +25,46 @@ var PplaBuilder = require('../lib/ppla-builder.js');
 exports.testSingleLineBuild = {
     setUp: function(done) {
         this.expectedFinalLabel = new Buffer('\u0002L\u000D4:5300101500010Etiqueta de teste\u000DE\u000D', 'ascii');
+        pplaBuilder.resetLabelConfig();
         done();
     },
     'no args': function(test) {
         test.expect(1);
 
-        PplaBuilder.rotation(PplaBuilder.DIRECTIONS.LANDSCAPE)
-                   .fontType(PplaBuilder.FONT_TYPE[':'].value)
-                   .fontSubType(PplaBuilder.COURIER_SUBTYPES.ECMA94)
+        pplaBuilder.rotation(pplaBuilder.DIRECTIONS.LANDSCAPE)
+                   .fontType(pplaBuilder.FONT_TYPE[':'].value)
+                   .fontSubType(pplaBuilder.COURIER_SUBTYPES.ECMA94)
                    .hScale(5)
                    .vScale(3)
                    .x(10)
                    .y(150)
                    .label('Etiqueta de teste');
 
-        test.deepEqual(PplaBuilder.build(), this.expectedFinalLabel, 'should be ' + this.expectedFinalLabel);
+        test.deepEqual(pplaBuilder.build(), this.expectedFinalLabel, 'should be ' + this.expectedFinalLabel);
+        test.done();
+    }
+};
+
+exports.testSingleLineBuildWithPixelSizeConfig = {
+    setUp: function(done) {
+        this.expectedFinalLabel = new Buffer('\u0002L\u000DD11\u000D4:5300101500010Etiqueta de teste\u000DE\u000D', 'ascii');
+        pplaBuilder.resetLabelConfig();
+        done();
+    },
+    'no args': function(test) {
+        test.expect(1);
+
+        pplaBuilder.setPixelSize(11)
+                   .rotation(pplaBuilder.DIRECTIONS.LANDSCAPE)
+                   .fontType(pplaBuilder.FONT_TYPE[':'].value)
+                   .fontSubType(pplaBuilder.COURIER_SUBTYPES.ECMA94)
+                   .hScale(5)
+                   .vScale(3)
+                   .x(10)
+                   .y(150)
+                   .label('Etiqueta de teste');
+
+        test.deepEqual(pplaBuilder.build(), this.expectedFinalLabel, 'should be ' + this.expectedFinalLabel);
         test.done();
     }
 };
@@ -47,30 +72,31 @@ exports.testSingleLineBuild = {
 exports.testMultiLineBuild = {
     setUp: function(done) {
         this.expectedFinalLabel = new Buffer('\u0002L\u000D4:5300101500010Etiqueta de teste\u000D2B220500200010098123456721\u000DE\u000D', 'ascii');
+        pplaBuilder.resetLabelConfig();
         done();
     },
     'no args': function(test) {
         test.expect(1);
 
-        PplaBuilder.rotation(PplaBuilder.DIRECTIONS.LANDSCAPE)
-            .fontType(PplaBuilder.FONT_TYPE[':'].value)
-            .fontSubType(PplaBuilder.COURIER_SUBTYPES.ECMA94)
+        pplaBuilder.rotation(pplaBuilder.DIRECTIONS.LANDSCAPE)
+            .fontType(pplaBuilder.FONT_TYPE[':'].value)
+            .fontSubType(pplaBuilder.COURIER_SUBTYPES.ECMA94)
             .hScale(5)
             .vScale(3)
             .x(10)
             .y(150)
             .label('Etiqueta de teste')
             .newLine()
-            .rotation(PplaBuilder.DIRECTIONS.REVERSE_LANDSCAPE)
+            .rotation(pplaBuilder.DIRECTIONS.REVERSE_LANDSCAPE)
             .barcodeType('B')
             .barcodeHeight('050')
-            .hScale(2)
-            .vScale(2)
+            .wideBar(2)
+            .narrowBar(2)
             .x(100)
             .y(200)
             .barcode('98123456721');
 
-        test.deepEqual(PplaBuilder.build(), this.expectedFinalLabel, 'should be ' + this.expectedFinalLabel);
+        test.deepEqual(pplaBuilder.build(), this.expectedFinalLabel, 'should be ' + this.expectedFinalLabel);
         test.done();
     }
 };
